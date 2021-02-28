863bfc899af49498151028f2168b16379c00a094
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-01 00:09:07 +0300

    Added new class

---
+++ Task/src/Result.java
@@ -0,0 +1,52 @@
+import java.util.Date;
+
+public class Result {
+    private String login;
+    private String test;
+    private Date date;
+    private int mark;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public Result(String login, String test, Date date, int mark) {
+        this.login = login;
+        this.test = test;
+        this.date = date;
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s;%s;%tD;%d",this.login, this.test, this.date, this.mark);
+    }
+}
