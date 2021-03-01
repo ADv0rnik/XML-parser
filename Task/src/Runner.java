d099d035353936940a6d2d9f7dbdfe245b2584d5
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-01 19:26:49 +0300

    Added validation

--- Task/src/Runner.java
+++ Task/src/Runner.java
@@ -1,5 +1,29 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
 public class Runner {
     public static void main(String[] args) {
-
+        System.out.println("Validation start...");
+        boolean answer = validateXMLSchema("results.xsd","results.xml");
+    }
+    public static boolean validateXMLSchema (String xsdPath, String xmlPath){
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(xsdPath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(xmlPath)));
+        } catch (IOException | SAXException e) {
+            System.out.println(e);
+            return false;
+        }
+        System.out.println("Validation Success");
+        return true;
     }
 }
