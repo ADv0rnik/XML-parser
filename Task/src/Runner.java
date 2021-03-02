2e7ad16d3bf433d42eeaba78ddc0ca4e29835544
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-02 13:50:47 +0300

    Corrections

--- Task/src/Runner.java
+++ Task/src/Runner.java
@@ -1,43 +1,27 @@
 import org.xml.sax.SAXException;
-
-import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Runner {
-    private static ArrayList<Result> results = new ArrayList<>();
+    private static final ArrayList<Result> results = new ArrayList<>();
+    static final String XML = "results.xml";
+    static final String XSD = "results.xsd";
     public static void main(String[] args) throws ParserConfigurationException, SAXException {
         System.out.println("Validation start...");
-        boolean answer = validateXMLSchema("results.xsd","results.xml");
+        FileValidator.validateXMLSchema(XSD,XML);
         SAXParserFactory factory = SAXParserFactory.newInstance();
         try {
             SAXParser parser = factory.newSAXParser();
             ResultHandler resultHandler = new ResultHandler();
-            parser.parse(new File("results.xml"), resultHandler);
-        } catch (IOException | SAXException e) {
+            parser.parse(new File(XML), resultHandler);
+        } catch (IOException e) {
             System.out.println("Parser open error: " + e.toString());
         }
     }
 
-    public static boolean validateXMLSchema (String xsdPath, String xmlPath){
-        try {
-            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new File(xsdPath));
-            Validator validator = schema.newValidator();
-            validator.validate(new StreamSource(new File(xmlPath)));
-        } catch (IOException | SAXException e) {
-            System.out.println("Validation failed with error. " + e.toString());
-            return false;
-        }
-        System.out.println("Validation Success");
-        return true;
-    }
+
 }
