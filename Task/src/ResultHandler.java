ff2d04ce7bb3726b8b784d8c88d386d7ba78a0bb
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-02 12:52:27 +0300

    Updated

--- Task/src/ResultHandler.java
+++ Task/src/ResultHandler.java
@@ -1,3 +1,6 @@
+import org.w3c.dom.ls.LSOutput;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class ResultHandler extends DefaultHandler {
@@ -6,7 +9,23 @@ public class ResultHandler extends DefaultHandler {
     static  final String TESTS_TAG = "tests";
     static  final String TEST_TAG = "test";
 
+    @Override
+    public void startDocument() throws SAXException {
+        System.out.println("Start parsing...");
+    }
 
+    @Override
+    public void endDocument() throws SAXException {
+        System.out.println("Parsing complete");
+    }
 
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        System.out.println("Start element: " + qName);
+    }
 
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        System.out.println("End element: " + qName);
+    }
 }
--- Task/src/Runner.java
+++ Task/src/Runner.java
@@ -1,6 +1,9 @@
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
@@ -11,15 +14,18 @@ import java.util.ArrayList;
 
 public class Runner {
     private static ArrayList<Result> results = new ArrayList<>();
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException {
         System.out.println("Validation start...");
         boolean answer = validateXMLSchema("results.xsd","results.xml");
-        ResultHandler resultHandler = new ResultHandler();
-
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try {
+            SAXParser parser = factory.newSAXParser();
+            ResultHandler resultHandler = new ResultHandler();
+            parser.parse(new File("results.xml"), resultHandler);
+        } catch (IOException | SAXException e) {
+            System.out.println("Parser open error: " + e.toString());
+        }
     }
-
-
-
 
     public static boolean validateXMLSchema (String xsdPath, String xmlPath){
         try {
@@ -28,7 +34,7 @@ public class Runner {
             Validator validator = schema.newValidator();
             validator.validate(new StreamSource(new File(xmlPath)));
         } catch (IOException | SAXException e) {
-            System.out.println(e);
+            System.out.println("Validation failed with error. " + e.toString());
             return false;
         }
         System.out.println("Validation Success");
